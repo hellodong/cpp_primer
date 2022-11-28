8ef0cf57f80700b70685532ca36a428bab8b552a
hellodong <hellodong@localhost.cn>
2022-11-28 23:31:29 +0800

    chapter 7 default construct

---
+++ Sales_data_default_construct/src/Sales_data.h
@@ -0,0 +1,23 @@
+
+#ifndef _SALES_DATA_H_
+#define _SALES_DATA_H_
+
+#include <iostream>
+#include <string>
+
+
+struct Sales_data{
+    Sales_data();
+    Sales_data(const std::string &s): bookNo(s) {}
+    std::string bookNo;
+    unsigned int units_sold=0;
+    double revenue = 0.0;
+};
+
+std::ostream &print(std::ostream &os, const Sales_data &entry);
+
+std::istream &read(std::istream &is, Sales_data &entry);
+
+
+#endif
+
