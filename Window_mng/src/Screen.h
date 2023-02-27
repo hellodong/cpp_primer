c938c4f093bc9bc9153977cb6068cfb1af7002a0
hellodong <hellodong@localhost.cn>
2023-02-27 19:22:44 +0800

    chapter 7, friend again

---
+++ Window_mng/src/Screen.h
@@ -0,0 +1,39 @@
+
+#ifndef _SCREEN_H_
+#define _SCREEN_H_
+
+#include <string>
+
+#include "Window_mng.h"
+
+
+class Screen{
+    friend void Window_mng::clear(ScreenIndex);
+    public:
+        using pos=std::string::size_type;
+        Screen() = default;
+        Screen(pos ht, pos wd, char c=' '):height(ht),width(wd),contents(ht * wd, c)
+        {}
+        Screen &set(char ch);
+        Screen &set(pos r, pos c, char ch);
+        Screen &display(std::ostream &os)
+        {
+            do_display(os);
+            return *this;
+        }
+        const Screen &display(std::ostream &os) const
+        {
+            do_display(os);
+            return *this;
+        }
+    private:
+        pos cursor = 0;
+        pos height = 0, width = 0;
+        std::string contents;
+        void do_display(std::ostream &os) const
+        {
+            os << contents;
+        }
+};
+
+#endif
---
+++ Window_mng/src/Window_mng.h
@@ -0,0 +1,52 @@
+
+
+#ifndef _WINDOW_MNG_H_
+#define _WINDOW_MNG_H_
+
+
+#include <vector>
+#include <string>
+
+
+class Screen;
+
+class Window_mng{
+    public:
+        using ScreenIndex = std::vector<Screen>::size_type;       
+        void clear(ScreenIndex);
+        Window_mng();
+    private:
+        std::vector<Screen> screens;
+};
+
+class Screen{
+    friend void Window_mng::clear(ScreenIndex);
+    public:
+        using pos=std::string::size_type;
+        Screen() = default;
+        Screen(pos ht, pos wd, char c=' '):height(ht),width(wd),contents(ht * wd, c)
+        {}
+        Screen &set(char ch);
+        Screen &set(pos r, pos c, char ch);
+        Screen &display(std::ostream &os)
+        {
+            do_display(os);
+            return *this;
+        }
+        const Screen &display(std::ostream &os) const
+        {
+            do_display(os);
+            return *this;
+        }
+    private:
+        pos cursor = 0;
+        pos height = 0, width = 0;
+        std::string contents;
+        void do_display(std::ostream &os) const
+        {
+            os << contents;
+        }
+};
+
+
+#endif
