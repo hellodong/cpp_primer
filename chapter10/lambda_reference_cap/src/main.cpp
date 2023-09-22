a0b88f9eb253ea12418b9b5afe930d488d468043
hellodong <hellodong@localhost.cn>
2023-09-22 10:01:24 +0800

    chapter10: lambda reference capture

---
+++ chapter10/lambda_reference_cap/src/main.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+	int input_val = 0;
+	//when create lambda, value of input_val pass to lambda
+	auto f=[&input_val] {return input_val;};
+	std::cin >> input_val;
+	std::cout << f() << std::endl; // cout is 0, f save the input_val when we create lambda
+	return 0;
+}
