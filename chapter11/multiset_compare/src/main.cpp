84db1b3a9546c0012bc850c2338418c944c1ec4c
hellodong <hellodong@localhost.cn>
2023-10-31 13:30:56 +0800

    chapter 11: markdown code link

--- chapter11/multiset_compare/src/main.cpp
+++ chapter11/multiset_compare/src/main.cpp
@@ -13,11 +13,11 @@ bool compareBookno(const Sales_data &lhs, const Sales_data &rhs)
 
 int main(int argc, char *argv[])
 {
-    //std::multiset<Sales_data, decltype(compareBookno) *>bookstore(compareBookno);
-    std::multiset<Sales_data, fnc>bookstore(compareBookno);
+    std::multiset<Sales_data, decltype(compareBookno) *>bookstore(compareBookno);
+    //std::multiset<Sales_data, fnc>bookstore(compareBookno);
 
     bookstore.insert(Sales_data("cpp primmer", 1, 128.00));
-    bookstore.insert(Sales_data("cpp_primmer", 2, 96.00));
+    bookstore.insert(Sales_data("cpp primmer", 2, 96.00));
     bookstore.insert(Sales_data("effective cpp", 1, 56.00));
     bookstore.insert(Sales_data("effective cpp", 10, 40.00));
 
