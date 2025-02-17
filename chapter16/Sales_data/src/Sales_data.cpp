a12d0ea5832368197bcf7d36665fd608f077cf07
hellodong <hellodong@localhost.cn>
2025-02-17 14:04:07 +0800

    chapter16: template specialization -> Sales_data

---
+++ chapter16/Sales_data/src/Sales_data.cpp
@@ -0,0 +1,69 @@
+
+#include "Sales_data.h"
+
+
+//output
+std::ostream& operator<<(std::ostream &os, const Sales_data &entry)
+{
+    os << "bookNo: " << entry.bookNo << ", units_sold: " << entry.units_sold << ", revenue: " << entry.revenue;
+    return os;
+}
+
+//input
+std::istream& operator>>(std::istream &is, Sales_data &rhm)
+{
+    double price = 0;
+    is >> rhm.bookNo >> rhm.units_sold >> price;
+    if (is)
+    {
+        rhm.revenue = rhm.units_sold * price;
+    }
+    else
+    {
+        rhm = Sales_data();
+    }
+
+    return is;
+}
+
+bool operator==(const Sales_data &lhs, const Sales_data &rhs)
+{
+    return lhs.bookNo == rhs.bookNo && lhs.units_sold == rhs.units_sold && lhs.revenue == rhs.revenue;
+}
+
+bool operator!=(const Sales_data &lhs, const Sales_data &rhs)
+{
+    return !(lhs == rhs);
+}
+
+Sales_data operator+(const Sales_data &lhm, const Sales_data &rhm)
+{
+    Sales_data sum(lhm);
+    sum += rhm;
+    return sum;
+}
+
+Sales_data& Sales_data::operator+=(const Sales_data &rhm)
+{
+    if (bookNo != rhm.bookNo)
+    {
+        return *this;
+    }
+
+    units_sold += rhm.units_sold;
+    revenue +=rhm.revenue;
+    return *this;
+}
+
+Sales_data& Sales_data::combine(const Sales_data &item)
+{
+    if (bookNo != item.bookNo)
+    {
+        return *this;
+    }
+    units_sold += item.units_sold;
+    revenue += item.revenue;
+
+    return *this;
+}
+
---
+++ chapter16/Sales_data/src/Sales_data.h
@@ -0,0 +1,49 @@
+
+#ifndef _SALES_DATA_H_
+#define _SALES_DATA_H_
+
+#include <iostream>
+#include <string>
+#include <functional>
+
+//template <class T> class std::hash;
+
+//template<> struct std::hash<Sales_data>;
+
+class Sales_data{
+    friend std::ostream & operator<<(std::ostream &os, const Sales_data &rhm);
+    friend std::istream & operator>>(std::istream &is, Sales_data &rhm);
+    friend Sales_data operator+(const Sales_data &lhm,const Sales_data &rhm);
+    friend bool operator==(const Sales_data &lhs, const Sales_data &rhs);
+    friend bool operator !=(const Sales_data &lhs, const Sales_data &rhs);
+    friend class std::hash<Sales_data>;
+public:
+	Sales_data(std::string s, unsigned int cnt, double price):bookNo(s),units_sold(cnt),revenue(cnt * price) {}
+	Sales_data():Sales_data("", 0,0.0) { }
+	explicit Sales_data(std::string s):Sales_data(s,0,0.0) { }
+
+	Sales_data &combine(const Sales_data &);
+    Sales_data &operator+=(const Sales_data &rhm);
+private:
+    std::string bookNo;
+    unsigned int units_sold=0;
+    double revenue = 0.0;
+};
+
+namespace std{
+    template<>
+    struct hash<Sales_data>
+    {
+        typedef size_t result_type;
+        typedef Sales_data argument_type;
+        size_t operator() (const Sales_data &s);
+    };
+    size_t hash<Sales_data>::operator()(const Sales_data &s)
+    {
+        return hash<std::string>()(s.bookNo) ^ hash<unsigned>()(s.units_sold) ^ hash<double>()(s.revenue);
+    }
+}
+
+
+#endif
+
